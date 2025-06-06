08b9bc323208ce98fc4a5bcf9eeb40416cb07e23
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-06-06 13:58:17 +0530

    Two Way Data binding using NgModel & user Method

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'AngularBasic-app';
+  courseName:String="B. tech Cse";
+ changeCourseName(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.courseName = input.value;
+  }
 }
