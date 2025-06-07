b564f0a50201e745d3037f8d3d0a2c746c837f9f
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-06-07 08:46:19 +0530

    signal Data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'AngularBasic-app';
+  dataBindingType = signal("Signal");
+  changeSignalState(){
+    this.dataBindingType.set("Signal is property work on it state ")
+  }
 }
