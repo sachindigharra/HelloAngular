7ab87e0ca26630fdfd7cb9fef8971fb729a95f87
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-06-12 15:21:53 +0530

    Routing and why its required

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StructuralDir } from './directive/structural-dir/structural-dir';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'structural-dir',
+        component:StructuralDir
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { StructuralDir } from './directive/structural-dir/structural-dir';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,StructuralDir],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'AngularBasic-app';
+  constructor(private routes:Router) {
+    
+  }
+  navigateToStructuralDir(){
+    // naviagate using ts function with constructor Dependency
+    this.routes.navigateByUrl('structural-dir')
+  }
 }
