3afe900f345e90f2fc8dff04b70957cc62b5e68c
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-06-20 07:31:31 +0530

    Custom Pipe and in-bulit Pipe

--- src/app/pipe/pipe.ts
+++ src/app/pipe/pipe.ts
@@ -1,11 +1,26 @@
+import { AsyncPipe, DatePipe, JsonPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { interval, map, Observable } from 'rxjs';
+import { NaPipe } from '../na-pipe';
 
 @Component({
   selector: 'app-pipe',
-  imports: [],
+  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,JsonPipe,AsyncPipe,NaPipe],
   templateUrl: './pipe.html',
   styleUrl: './pipe.css'
 })
 export class Pipe {
-
+  name: string = 'SAchin SHarma';
+  temp:any='';
+  
+  currentDate:Date = new Date();
+  Student:any={
+    Name:'sachin sharma',
+    Education:'B.tech',
+    Branch:'CSE'
+  }
+  currentTime:Observable<Date>=new Observable<Date>;
+  constructor(){
+    this.currentTime = interval(1000).pipe(map(() => new Date()))
+  }
 }
