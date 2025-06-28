47a4003188d4e3d778cabf0f1527c54b94b978c1
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-06-28 22:11:09 +0530

    correct app.ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Pipe } from './pipe/pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Pipe],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
